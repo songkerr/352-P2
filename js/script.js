614dff9666f32ea7cf40e303babc32e015904674
Kerryn Song <kerrynsong@Kerryns-MacBook-Pro.local>
2018-04-23 23:22:46 +1200

    basic data submit with button

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var mainText = document.getElementById("mainText");
+var submitBtn = document.getElementById("submitBtn");
+
+function submitClick() {
+    var firebaseRef = firebase.database().ref();
+    firebaseRef.child("Text").set("Value");
+}
