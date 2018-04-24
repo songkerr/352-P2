1862952328c828e4c3ac51581fbdc9f56cc2dee7
Kerryn Song <kerrynsong@Kerryns-MacBook-Pro.local>
2018-04-24 19:19:32 +1200

    datalist

--- js/script.js
+++ js/script.js
@@ -1,7 +1,28 @@
 var mainText = document.getElementById("mainText");
 var submitBtn = document.getElementById("submitBtn");
+var breed = document.getElementById("breed");
+
+var breedRef = firebase.database().ref().child("Breed");
+
+breedRef.on('value', function(snapshot){
+    breed.innerText = snapshot.val();
+});
 
 function submitClick() {
     var firebaseRef = firebase.database().ref();
-    firebaseRef.child("Text").set("Value");
+    
+    var messageText = mainText.value;
+    
+    firebaseRef.push().set(messageText);
 }
+
+//  $(function() {
+//    var availableTags = [
+//      "Husky",
+//      "Labrador",
+//      "Chihuahua"
+//    ];
+//    $( "#breeds" ).autocomplete({
+//      source: availableTags
+//    });
+//  } );
