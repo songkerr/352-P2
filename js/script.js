9a72ae98413ccdb797d37f17267759f8334835c3
Kerryn Song <kerrynsong@Kerryns-MacBook-Pro.local>
2018-04-24 21:22:46 +1200

    sdgfhfh

--- js/script.js
+++ js/script.js
@@ -1,21 +1,37 @@
-var mainText = document.getElementById("mainText");
-var submitBtn = document.getElementById("submitBtn");
-var breed = document.getElementById("breed");
-
-var breedRef = firebase.database().ref().child("Breed");
-
-breedRef.on('value', function(snapshot){
-    breed.innerText = snapshot.val();
+$(document).ready(function () {
+    
+    var rootRef = firebase.database().ref().child("Tracks");
+    
+    rootRef.on("child_added", snap =>{
+        
+        var size = snap.child("Size").val();
+        
+        $("#sizeValue").append("<input list><datalist><option value=" + size + "></option></datalist></input>");
+        
+    });
+    
 });
+//"<div>" + size + "</div>"
 
-function submitClick() {
-    var firebaseRef = firebase.database().ref();
-    
-    var messageText = mainText.value;
-    
-    firebaseRef.push().set(messageText);
-}
 
+//var mainText = document.getElementById("mainText");
+//var submitBtn = document.getElementById("submitBtn");
+//var size = document.getElementById("size");
+//
+//var breedRef = firebase.database().ref().child("Large");
+//
+//breedRef.on('value', function(snapshot){
+//    breed.innerText = snapshot.val();
+//});
+//
+//function submitClick() {
+//    var firebaseRef = firebase.database().ref();
+//    
+//    var messageText = mainText.value;
+//    
+//    firebaseRef.push().set(messageText);
+//}
+//
 //  $(function() {
 //    var availableTags = [
 //      "Husky",
